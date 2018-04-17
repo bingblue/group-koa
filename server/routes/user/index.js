626910402f23f3b3fc77b2198a12995aded38455
gavin <gavin.gao@e3-expo.com>
2018-04-17 14:48:17 +0800

    test jenkins

--- server/routes/user/index.js
+++ server/routes/user/index.js
@@ -6,7 +6,7 @@ router.get('/', function (ctx, next) {
 })
 
 router.get('/bar', function (ctx, next) {
-  ctx.body = 'this is a users/barr response'
+  ctx.body = 'this is a users/bar response'
 })
 
 module.exports = router
